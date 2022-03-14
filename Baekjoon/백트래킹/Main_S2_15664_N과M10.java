03e98f05db3a709a4e9f362e9bfb6980961a0662
김광희 <hgi7201@naver.com>
2022-03-14 22:32:14 +0900

    add: 조합을 이용하여 해결

---
+++ Baekjoon/백트래킹/Main_S2_15664_N과M10.java
@@ -0,0 +1,42 @@
+package BOJ;
+import java.io.*;
+import java.util.*;
+
+// 조합 문제
+public class Main_S2_15664_N과M10 {
+	static int N,M ;
+	static int [] inputs,numbers;
+	static LinkedHashSet<String> set ;
+	public static void main(String[] args) throws Exception {
+//		System.setIn(new FileInputStream("res/input1.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		set =  new LinkedHashSet<String>();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken()); // 전체 원소
+		M = Integer.parseInt(st.nextToken()); // 꺼낼 원소 수.
+		inputs = new int[N];
+		numbers = new int[M];
+		
+		st = new StringTokenizer(br.readLine()," ");
+		for (int i = 0; i < N; i++) inputs[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(inputs);// 수열은 사전 순으로 증가하는 순서로 출력해야해서 미리 오름차순으로 수열을 정렬
+		comb(0,0);
+		
+		StringBuilder sb = new StringBuilder();
+		for (String line : set) sb.append(line).append("\n");
+		System.out.println(sb);// 최종 출력.
+	}
+	public static void comb(int depth, int start) {
+		if(depth == M) {
+			StringBuilder sb = new StringBuilder();
+			for (int num : numbers) sb.append(num).append(" ");
+			set.add(sb.toString()); // 중복값이 있을경우 안 넣음.
+			return;
+		}
+		for (int i = start; i < N; i++) {
+			numbers[depth] = inputs[i];
+			comb(depth+1, i+1);
+		}
+	}
+}
