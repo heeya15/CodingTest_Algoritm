240381df4bd7fcc7f14fe50cbb8a20f769ba25fb
김광희 <hgi7201@naver.com>
2022-05-01 16:56:36 +0900

    add: 주어진 조건에 따른 처리로 해결

---
+++ Baekjoon/구현/Main_B4_2753_윤년.java
@@ -0,0 +1,11 @@
+package BOJ;
+import java.util.Scanner;
+public class Main_B4_2753_윤년 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		if(N % 4 ==0 && N %100 != 0 || N %400 ==0)System.out.println(1);
+		else System.out.println(0);
+	}
+
+}
