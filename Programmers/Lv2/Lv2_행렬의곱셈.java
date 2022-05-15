6ff2941521a0931167b5351b78c006397754fb27
김광희 <hgi7201@naver.com>
2022-05-15 23:55:04 +0900

    add : 행렬의 곱셈을 구현 반복문만 이용
    - 코드로 행렬의 곱셈을 옮기려하니 잘 구현되지 않았다
    - 메모장에 하나하나 적어보며 해당 계산과정을 코드로 옮김

---
+++ Programmers/Lv2/Lv2_행렬의곱셈.java
@@ -0,0 +1,21 @@
+package Programmers;
+
+import java.util.*;
+public class Lv2_행렬의곱셈 {
+	public static void main(String[] args) {
+		int arr1[][] = { { 1, 4 }, { 3, 2 }, { 4, 1 } };
+		int arr2[][] = { { 3, 3 }, { 3, 3 } };
+		System.out.println(Arrays.deepToString(solution(arr1, arr2)));
+	}
+	public static int[][] solution(int[][] arr1, int[][] arr2) {
+		int[][] answer = new int[arr1.length][arr2[0].length];
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr2[0].length; j++) {
+				for (int k = 0; k < arr1[0].length; k++) {
+					answer[i][j] += arr1[i][k] * arr2[k][j];
+				}
+			}
+		}
+		return answer;
+	}
+}
