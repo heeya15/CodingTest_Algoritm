e6c69507ee010994bc5c3bc607655105f2f9458a
hgi7201 <hgi7201@naver.com>
2022-07-31 20:35:49 +0900

    add : 기본 출력문 처리 문제

---
+++ Baekjoon/구현/Main_B5_10172_개.java
@@ -0,0 +1,13 @@
+package 구현;
+
+public class Main_B5_10172_개 {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("|\\_/|").append("\n").
+		append("|q p|   /}").append("\n").
+		append("( 0 )\"\"\"\\").append("\n").
+		append("|\"^\"`    |").append("\n").
+		append("||_/=\\\\__|").append("\n");
+		System.out.println(sb.toString());
+	}	
+}
